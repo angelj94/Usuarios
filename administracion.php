542c61f538765efb5cc2e6c3f7a2cfb52c7b6bbb
juampi-d <juampi1732@gmail.com>
2023-04-04 01:36:54 -0300

    Funciones agregadas.
    Funciones funcionando correctamente.

--- administracion.php
+++ administracion.php
@@ -2,50 +2,119 @@
 
 include('conexion.php');
 session_start();
-$queryusuario 	= mysqli_query($conn,"SELECT * FROM usuarios NATURAL JOIN roles");
-$mostrar	= mysqli_fetch_all($queryusuario); 
 if (!$_SESSION['rolusuario'] =='admin') {
     header("location: principal.php");
 }
+$SelectedRol =0;
+$selectedID = 0;
+$selectedNombre = '';
+$selectedApellido = '';
+$selectedCorreo = '';
+
 if (isset($_POST['metodo'])) {
     switch ($_POST['metodo']) {
         case 'Selecionar':
-            # code...
-            break;
+            $querymetodo = mysqli_query($conn,"SELECT * FROM usuarios NATURAL JOIN roles WHERE usuarios_id = '$_POST[usuario_id]';");
+            $selected	= mysqli_fetch_array($querymetodo); 
+            if (isset($selected)) {
+                $selectedID = $selected['usuarios_id'];
+                $selectedNombre = $selected['nombre'];
+                $selectedApellido = $selected['apellido'];
+                $selectedCorreo = $selected['correo'];
+                $selectedRol = $selected['rol_id'];		
+            }
+        break;
         case 'Editar':
-            # code...
-            break;
+            $querymetodo = mysqli_query($conn,"UPDATE `usuarios` SET `nombre` = '$_POST[nombre]', `apellido` = '$_POST[apellido]', `correo` = '$_POST[correo]', `rol_id` = '$_POST[rol]' WHERE `usuarios`.`usuarios_id` = '$_POST[usuario_id]';");
+        break;
         case 'Borrar':
-            # code...
-            break;
+            $querymetodo = "DELETE FROM usuarios WHERE usuarios_id = '$_POST[usuario_id]';";
+            $conn->query($querymetodo);
+        break;
         case 'Cancelar':
-            # code...
-            break;
+            $selectedID = 0;
+            $selectedNombre = "";
+            $selectedApellido = "";
+            $selectedCorreo = "";
+            $selectedRol = "";
+        break;
     }
 }
 ?>
-<html>
+<html lang="es">
 
-<head>
-    <meta charset="UTF-8">
-    <title>Administrador</title>
-    <link rel="stylesheet" href="login.css">
-</head>
-
-<body>
+    <head>
+        <meta charset="UTF-8">
+        <title>Administrador</title>
+        <link rel="stylesheet" href="login.css">
+    </head>
+    
+    <body>
     <div class="cajafuera">
-    <?php 
-    foreach ($mostrar as $usuario) {
-        echo "<br>rol_id = ".$usuario[0];
-        echo "<br>usuario_id = ".$usuario[1];
-        echo "<br>nombre = ".$usuario[2];
-        echo "<br>apellido = ".$usuario[3];
-        echo "<br>correo = ".$usuario[4];
-        echo "<br>contraseña(hash) = ".$usuario[5];
-        echo "<br>rol = ".$usuario[6];
-        echo "<br>";
-    }
-    ?>
+        <form action="administracion.php" method="post">
+            <label for="usuario_id">ID de usuario</label>
+            <input type="number" name="usuario_id" id="" readonly <?php echo"value='$selectedID'";?>>
+            <label for="nombre">Nombre</label>
+            <input type="text" name="nombre" id="" <?php echo"value='$selectedNombre'";?>>
+            <label for="apellido">Apellido</label>
+            <input type="text" name="apellido" id="" <?php echo"value='$selectedApellido'";?>>
+            <label for="correo">Correo</label>
+            <input type="text" name="correo" id="" <?php echo"value='$selectedCorreo'";?>>
+            <label for="rol">Rol</label>
+            <select name="rol" id="">
+                <?php
+                $queryrol 	= mysqli_query($conn,"SELECT * FROM roles;");
+                $roles	= mysqli_fetch_all($queryrol);
+                foreach ($roles as $key => $value) {
+                    echo "<option value='$value[0]'";
+                    if (isset($selectedRol)) {
+                        if ($selectedRol==$value[0]) {
+                            echo " selected";
+                        }else{
+                            echo"";
+                        }
+                    }
+                    echo ">$value[1]</option>";
+                }
+                ?>
+            </select>
+            <input type="submit" name='metodo' value="Editar">
+            <input type="submit" name='metodo' value="Cancelar">
+        </form>
+        <br>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nombre y apellido</th>
+                    <th>Correo</th>
+                    <th>Rol</th>
+                    <th>Acción</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php 
+                    $queryusuario 	= mysqli_query($conn,"SELECT * FROM usuarios NATURAL JOIN roles WHERE usuarios_id != 1;");
+                    $mostrar	    = mysqli_fetch_all($queryusuario); 
+                foreach ($mostrar as $key => $usuario) {
+                    echo "
+                    <tr>
+                    <td>$usuario[1]</td>
+                    <td>$usuario[2] $usuario[3]</td>
+                    <td>$usuario[4]</td>
+                    <td>$usuario[6]</td>
+                        <td>
+                            <form action='administracion.php' method='post'>
+                                <input type='hidden' name='usuario_id' value='$usuario[1]'>
+                                <input type='submit' name='metodo' value='Selecionar'>
+                                <input type='submit' name='metodo' value='Borrar'>
+                            </form>
+                        </td>
+                    </tr>";
+                }
+                ?>
+            </tbody>
+        </table>
     </div>
     
 </body>
